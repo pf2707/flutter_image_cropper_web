8f22457a0b253f739af1a50798cd0b4d5585b78f
scofieldtran <pf2707@gmail.com>
2025-03-21 12:53:44 +0700

    export neccessary files

--- image_cropper_for_web/lib/image_cropper_for_web.dart
+++ image_cropper_for_web/lib/image_cropper_for_web.dart
@@ -1,5 +1,27 @@
 library image_cropper_for_web;
 
+export 'package:image_cropper_platform_interface/image_cropper_platform_interface.dart'
+    show
+    CropAspectRatioPresetData,
+    CropAspectRatioPreset,
+    CropStyle,
+    ImageCompressFormat,
+    CropAspectRatio,
+    CroppedFile,
+    RotationAngle,
+    PlatformUiSettings,
+    AndroidUiSettings,
+    IOSUiSettings,
+    WebUiSettings,
+    WebDialogBuilder,
+    WebRouteBuilder,
+    WebPresentStyle,
+    WebThemeData,
+    WebTranslations,
+    CropperSize,
+    WebDragMode,
+    WebViewMode;
+
 import 'dart:async';
 import 'dart:convert';
 import 'dart:js_interop';
